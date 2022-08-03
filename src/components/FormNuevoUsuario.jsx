9a7d4929c9980276ff76640b86d899ae3c083aa1
Madmonkey08 <ajtm082698@gmail.com>
2022-08-03 11:40:53 -0500

    Subiendo correcciones de estilos

--- src/components/FormNuevoUsuario.jsx
+++ src/components/FormNuevoUsuario.jsx
@@ -69,34 +69,34 @@ export const FormNuevoUsuario = ({ setFormulario, bandera, setBandera }) => {
                     type="number"
                     value={cedula}
                     onChange={(e) => setCedula(e.target.value)}
-                    className="outline-none rounded-lg pl-3 bg-transparent text-white py-1 w-full dark:bg-gray-800"
+                    className="outline-none rounded-lg pl-3 bg-transparent text-white py-1 w-full bg-gray-800"
                 />
                 <label className="text-white block mt-4 w-full">Nombre:</label>
                 <input
                     value={nombre}
                     onChange={(e) => setNombre(e.target.value)}
-                    className="outline-none rounded-lg pl-3 bg-transparent text-white py-1 w-full dark:bg-gray-800"
+                    className="outline-none rounded-lg pl-3 bg-transparent text-white py-1 w-full bg-gray-800"
                 />
                 <label className="text-white block mt-4 w-full">Contraseña:</label>
                 <input
                     type="password"
                     value={contrasena}
                     onChange={(e) => setContrasena(e.target.value)}
-                    className="outline-none rounded-lg pl-3 bg-transparent text-white py-1 w-full dark:bg-gray-800"
+                    className="outline-none rounded-lg pl-3 bg-transparent text-white py-1 w-full bg-gray-800"
                 />
                 <label className="text-white block mt-4 w-full">Confirmar contraseña:</label>
                 <input
                     type="password"
                     value={confirmContra}
                     onChange={(e) => setConfirmContra(e.target.value)}
-                    className="outline-none rounded-lg pl-3 bg-transparent text-white py-1 w-full dark:bg-gray-800"
+                    className="outline-none rounded-lg pl-3 bg-transparent text-white py-1 w-full bg-gray-800"
                 />
                 <label className="text-white block mt-4 w-full">Monto:</label>
                 <input
                     type="number"
                     value={monto}
                     onChange={(e) => setMonto(e.target.value)}
-                    className="outline-none rounded-lg pl-3 bg-transparent text-white py-1 w-full dark:bg-gray-800"
+                    className="outline-none rounded-lg pl-3 bg-transparent text-white py-1 w-full bg-gray-800"
                 />
                 <div className="flex mt-8">
                     <input type="submit" id="btnIngresar" className="rounded-lg text-center px-3 outline-none py-1 mr-6 cursor-pointer" value="Registrar" />
